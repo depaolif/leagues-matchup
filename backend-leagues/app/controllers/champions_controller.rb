278cff7381399b4c750eaff2281546cee0c2ba59
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-17 15:48:18 -0400

    added paths to get images and enemytips

--- backend-leagues/app/controllers/champions_controller.rb
+++ backend-leagues/app/controllers/champions_controller.rb
@@ -3,14 +3,16 @@ class ChampionsController < ApplicationController
 
 	def create
 		# params should have the champion's name and the champion's party
-		id = getChampionId(params[:name].titleize)
+		id = getChampionId(params[:name])
 		byebug
 		party = params["party"]
-		if party == "enemy" 
-			# get enemy tips from url
+		# if party == "enemy" 
+		enemytips = getChampionEnemyTips(id)
+		# end
+		loading_splash, icon = getChampionImages(name).values_at(:loading_splash, :icon)
+		byebug
 		# champion = Champion.find_or_create_by(id: id)
 	end
 
 end
-
 
--- backend-leagues/app/helpers/champions_helper.rb
+++ backend-leagues/app/helpers/champions_helper.rb
@@ -1,6 +1,15 @@
 module ChampionsHelper
-	def getChampionInfo(id)
-		# RestClient.get "http"
+	def getChampionEnemyTips(id)
+		response = RestClient.get "https://na1.api.riotgames.com/lol/static-data/v3/champions/266?champData=all&api_key=RGAPI-17b84d33-1b75-47b4-b5d7-ac9004579f30"
+		enemytips = JSON.parse(response.body)["enemytips"]
+		enemytips
+	end
+
+	def getChampionImages(name)
+		loading_splash = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/#{name}_0.jpg"
+		icon = "http://ddragon.leagueoflegends.com/cdn/#{$ddragon_version}/img/champion/#{name}.png"
+		byebug
+		{:loading_splash => loading_splash, :icon => icon}
 	end
 
 	def getChampionId(name)
